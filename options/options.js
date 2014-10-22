2e9385f6999e1d64f646a97cc7e8ee8b17d9fad9
Jeroen Visser <jeroenvisser101@gmail.com>
2014-10-22 07:52:29 +0200

    Added restore button

--- options/options.js
+++ options/options.js
@@ -1,22 +1,46 @@
 (function (document, window, chrome) {
     var save_button = document.getElementById('save-button'),
+        reset_button = document.getElementById('reset-button'),
         pr_template = document.getElementById('pr-template'),
-        save_status = document.getElementById('save-status');
+        save_status = document.getElementById('save-status'),
+        reset_to_v1 = document.getElementById('reset-to-v1');
 
     restore_settings();
     save_button.addEventListener('click', save_settings);
+    reset_button.addEventListener('click', function (){
+        pr_template.value = GitFlow.pr_template;
+        save_settings();
+    });
+    reset_to_v1.addEventListener('click', restore_v1_settings);
 
+    /**
+     * Saves the settings into local storage.
+     */
     function save_settings() {
         chrome.storage.sync.set({
-            pr_template: pr_template.value
+            pr_template_v2: pr_template.value
         }, function (items) {
             save_status.textContent = 'Options saved!';
         });
     }
 
+    /**
+     * Restores the previously set settings.
+     */
     function restore_settings() {
         chrome.storage.sync.get({
-            pr_template:  "#### What does this PR do?\nWhich problem(s) does it fix and how? What does this PR add?\n\n#### Where should the reviewer start?\nExplain what should be set-up first in order to get the review started. Which page has to be viewed etc.\n\n#### How should this be manually tested?\n- [ ] Write down all\n- [ ] the steps a person\n- [ ] needs to take to test it.\n\n##### Screenshots (if appropriate)\nIf this PR changes something that concerns UI changes, please post before and after screenshots here.\n\n#### Who should be notified?\n- [ ] Write down the name/department this branch needs\n- [ ] to be communicated to\n- [ ] Check the box(es) to indicate that it has been\n- [ ] communicated (add the label)\n\n#### What should happen on deployment? (check all that apply)\n- [x] The usual steps.\n- [ ] There are database changes, which should be run.\n- [ ] There are files that should be manually uploaded.\n\n#### Questions:\n- Does this require a blog post?\n- Does this require a knowledge base update?\n- Does support need training for this?\n- Does this has to be communicated to partners?"
+            pr_template_v2: GitFlow.pr_template
+        }, function (items) {
+            pr_template.value = items.pr_template || GitFlow.pr_template;
+        });
+    }
+
+    /**
+     * Restores the settings used in version 1 of this application.
+     */
+    function restore_v1_settings() {
+        chrome.storage.sync.get({
+            pr_template: GitFlow.pr_template
         }, function (items) {
             pr_template.value = items.pr_template;
         });
