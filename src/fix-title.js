139f213bdd084703a90fc67a9953a629a82e11e9
Jeroen Visser <jeroenvisser101@gmail.com>
2014-10-22 07:54:35 +0200

    Fix for title bug, only happens at pageload, not before.

--- src/fix-title.js
+++ src/fix-title.js
@@ -1,16 +1,25 @@
 (function (document, window, undefined) {
-    // First, check if we have to do anything
-    if (window.location.href.match(/\/compare\//i)) {
-        // Get the title and comment field:
-        var title_field   = document.querySelector('[name="pull_request[title]"]'),
-            comment_field = document.querySelector('[name="pull_request[body]"]');
+    function initFixTitle() {
+        // First, check if we have to do anything
+        if (window.location.href.match(/\/compare\//i)) {
+            // Get the title and comment field:
+            var title_field = document.querySelector('[name="pull_request[title]"]'),
+                comment_field = document.querySelector('[name="pull_request[body]"]');
 
-        // Remove the 'Hotfix/' or 'Feature/' prefix from the title field
-        GitFlow.fix_title(title_field);
+            // Remove the 'Hotfix/' or 'Feature/' prefix from the title field
+            GitFlow.fix_title(title_field);
 
-        // Check if the comment field is empty
-        if (comment_field.value == '') {
-            GitFlow.insert_pr_template(comment_field);
+            // Check if the comment field is empty
+            if (comment_field.value == '') {
+                GitFlow.insert_pr_template(comment_field);
+            }
         }
+    }
+
+    // Make sure this only happens at the right time!
+    if (document.readyState === 'complete' || document.readyState === 'interactive') {
+        initFixTitle();
+    } else {
+        document.addEventListener('load', initFixTitle);
     }
 })(document, window);
